83b6eaf60a0051e7f9881a11149b3d309685db55
Davor Cortés <davorcortes.dc@gmail.com>
2020-09-26 00:00:35 -0500

    conexión con base de datos

---
+++ src/main/java/edu/eci/arsw/mediameet/persistence/rooms/RoomRepository.java
@@ -0,0 +1,11 @@
+package edu.eci.arsw.persistence.rooms;
+
+import edu.eci.arsw.mediameet.model.Room;
+
+@
+public interface RoomRepository {
+
+    public Room saveRoom(Room room);
+
+
+}
---
+++ src/main/java/edu/eci/arsw/mediameet/persistence/rooms/impl/RoomRepositoryImpl.java
@@ -0,0 +1,11 @@
+package edu.eci.arsw.persistence.rooms.impl;
+
+import edu.eci.arsw.mediameet.model.Room;
+import edu.eci.arsw.persistence.rooms.RoomRepository;
+
+public class RoomRepositoryImpl implements RoomRepository {
+
+    public Room saveRoom(Room room){
+        return null;
+    }
+}
