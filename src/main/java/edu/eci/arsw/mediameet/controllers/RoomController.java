a7a77d77a28da5a9697b2952bfacb3ce18eb7947
srubianof <srubianof@icloud.com>
2020-10-06 00:51:59 -0500

    Update RoomController.java

--- src/main/java/edu/eci/arsw/mediameet/controllers/RoomController.java
+++ src/main/java/edu/eci/arsw/mediameet/controllers/RoomController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"http://localhost:4200","https://media-meet.web.app"})
+@CrossOrigin(origins = {"https://localhost:4200","https://media-meet.web.app"})
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
