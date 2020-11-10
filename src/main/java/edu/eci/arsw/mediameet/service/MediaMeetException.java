9fe0e9581b1286e54bf20809cc6f9dffad1aa6b6
D4v0r <davorcortes.dc@gmail.com>
2020-11-09 22:57:42 -0500

    AddNewRoom Service

--- src/main/java/edu/eci/arsw/mediameet/service/MediaMeetException.java
+++ src/main/java/edu/eci/arsw/mediameet/service/MediaMeetException.java
@@ -3,6 +3,7 @@ package edu.eci.arsw.mediameet.service;
 public class MediaMeetException extends Exception{
     public static final String NOT_VIDEOS_FOUND = "NOT_VIDEOS_FOUND";
     public static final String RESOURCE_NOT_FOUND  = "RESOURCE_NOT_FOUND";
+    public static final String REPEAT_ROOM = "THE USER ALREADY BELONGS TO THAT ROOM.";
     public MediaMeetException(String message){
         super(message);
     }
--- src/main/java/edu/eci/arsw/mediameet/service/profileServices/impl/ProfileServicesImpl.java
+++ src/main/java/edu/eci/arsw/mediameet/service/profileServices/impl/ProfileServicesImpl.java
@@ -1,6 +1,7 @@
 package edu.eci.arsw.mediameet.service.profileServices.impl;
 
 import edu.eci.arsw.mediameet.model.Profile;
+import edu.eci.arsw.mediameet.model.Room;
 import edu.eci.arsw.mediameet.persistence.rooms.ProfileRepository;
 import edu.eci.arsw.mediameet.service.MediaMeetException;
 import edu.eci.arsw.mediameet.service.profileServices.ProfileServices;
@@ -31,6 +32,16 @@ public class ProfileServicesImpl implements ProfileServices {
     @Override
     public List<Profile> loadProfiles() {
         return repository.findAll();
+    }
+
+    @Override
+    public void addNewRoom(String id, Room room) throws MediaMeetException {
+        Profile profile = loadById(id);
+        if(profile.getRooms().stream().anyMatch(r -> r.getId().equals(room.getId()))) throw new MediaMeetException(MediaMeetException.REPEAT_ROOM);
+        List<Room> rooms = profile.getRooms();
+        rooms.add(room);
+        profile.setRooms(rooms);
+        save(profile);
     }
 
     @Override
