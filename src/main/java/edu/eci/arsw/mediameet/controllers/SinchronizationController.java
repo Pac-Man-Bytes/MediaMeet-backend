2c5c74e4370aa898e1adf285c01dc30656692218
Santiago <srubianof@icloud.com>
2020-11-10 23:17:19 -0500

    chat profile updated

--- src/main/java/edu/eci/arsw/mediameet/controllers/SinchronizationController.java
+++ src/main/java/edu/eci/arsw/mediameet/controllers/SinchronizationController.java
@@ -1,7 +1,6 @@
 package edu.eci.arsw.mediameet.controllers;
 
 
-
 import edu.eci.arsw.mediameet.model.Media;
 import edu.eci.arsw.mediameet.model.Room;
 import edu.eci.arsw.mediameet.model.Video;
@@ -13,6 +12,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
 import java.util.List;
 
 @Controller
@@ -21,21 +21,25 @@ public class SinchronizationController {
     RoomServices roomServices;
     @Autowired
     private SimpMessagingTemplate webSocket;
+
     @MessageMapping("/state/{roomId}")
     @SendTo("/room/state/{roomId}")
     public String recibirMensaje(@DestinationVariable String roomId, String message) {
         return message;
     }
+
     @MessageMapping("/videoStatus/{roomId}")
     @SendTo("/room/videoStatus/{roomId}")
     public Media recibirMensaje(@DestinationVariable String roomId, Video currentTrack) {
         return currentTrack;
     }
+
     @MessageMapping("/next/{roomId}")
     @SendTo("/room/next/{roomId}")
     public Media recibirMensajeDevolver(@DestinationVariable String roomId, Video currentTrack) {
         return currentTrack;
     }
+
     //Guarda
     @MessageMapping("/queue/{roomId}")
     public void receiveMedia(@DestinationVariable String roomId, Video currentTrack) throws MediaMeetException {
@@ -44,21 +48,25 @@ public class SinchronizationController {
         roomServices.save(room);
         webSocket.convertAndSend("/room/queue/" + roomId, room.getPlaylist());
     }
+
     @MessageMapping("/queue/{roomId}/playlist")
     public void getMedia(@DestinationVariable String roomId) throws MediaMeetException {
         Room room = roomServices.loadById(roomId);
         webSocket.convertAndSend("/room/queue/" + roomId + "/playlist", room.getPlaylist());
     }
+
     @MessageMapping("/fetch/{roomId}")
     public void fetchMedia(@DestinationVariable String roomId) throws MediaMeetException {
         Room room = roomServices.loadById(roomId);
         webSocket.convertAndSend("/room/fetch/" + roomId, room.getId());
     }
+
     @MessageMapping("/currentTime/{roomId}")
     @SendTo("/room/currentTime/{roomId}")
     public Video time(@DestinationVariable String roomId, Video currentTrack) {
         return currentTrack;
     }
+
     //Obtener cola actual
     //actualiza cola actual
     @MessageMapping("/queue/{roomId}/playlists")
